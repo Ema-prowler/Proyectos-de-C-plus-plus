e709c25a631c11d7fdc5c3f9fe8e5a0678aec8ea
Emanuel Coletti <emanuelcoletti001@gmail.com>
2023-11-29 21:12:54 -0300

    Mejorando algunos aspectos del juego y agregando un mensaje multicolor al final

--- Juego del ahorcado/Juego-del-ahorcado.cpp
+++ Juego del ahorcado/Juego-del-ahorcado.cpp
@@ -1,10 +1,20 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <string>
+#include <windows.h>
+
 using namespace std;
+// Funciones
+void borrarPantalla() { system("cls"); }
+
+void cambiarColor(int color) {
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(hConsole, color);
+}
 
 void mostrarEstadoJuego(const string &palabraOculta, int intentosRestantes) {
   cout << "Palabra: " << palabraOculta << endl;
-
   cout << "Intentos restantes: " << intentosRestantes << endl;
 }
 int main() {
@@ -12,26 +22,38 @@ int main() {
   string palabraAdivinar;
   char letraAdivinada;
   int intentosRestantes = 6;
+  srand(static_cast<unsigned int>(time(0)));
+  string textoACambiar = palabraAdivinar;
+  int fila = 1;
+  int columna = 1;
+
   cout << "Bienvenido al Juego del ahorcado. " << endl;
   cout << "Jugador 1, Por favor ingrese la palabra a adivinar: ";
   cin >> palabraAdivinar;
   system("cls");
+
   // paso 2: Mostrar la palabra a adivinar (esto es olo para verificar que la
   // entrada funciono)
+
   cout << "La palabra a adivinar es: " << palabraAdivinar << endl;
   string palabraOculta(palabraAdivinar.length(), '_');
   cout << "Palabra Oculta: " << palabraOculta << endl;
+
   while (intentosRestantes > 0) {
+
     mostrarEstadoJuego(palabraOculta, intentosRestantes);
     cout << "jugador 2 por favor ingrese una letra: ";
     cin >> letraAdivinada;
+
     bool letraEncontrada = false;
+
     for (int i = 0; i < palabraAdivinar.length(); i++) {
       if (palabraAdivinar[i] == letraAdivinada) {
         palabraOculta[i] = letraAdivinada;
         letraEncontrada = true;
       }
     }
+
     if (letraEncontrada) {
       system("cls");
       cout << "Correcto la letra esta en la palabra." << endl;
@@ -40,10 +62,20 @@ int main() {
       cout << "Incorrecto madafakaa no esta la letra jaja" << endl;
       intentosRestantes--;
     }
+
     if (palabraOculta == palabraAdivinar) {
       mostrarEstadoJuego(palabraOculta, intentosRestantes);
       cout << "felicidades flaco lo lograste adivinaste la palabra." << endl;
-      break;
+      Sleep(2000);
+      borrarPantalla();
+
+      while (true) {
+        int color = rand() % 15 + 1;
+        cambiarColor(color);
+        cout << "\033[" << fila << ";" << columna << "H"
+             << "Palabra adivinada: " << palabraAdivinar << endl;
+        Sleep(200);
+      }
     }
   }
   if (intentosRestantes == 0) {
